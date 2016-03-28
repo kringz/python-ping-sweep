205ae41e47b9e20c9a509b4be7ba815cd98aeae6
root <krings.stephen@gmail.com>
2016-03-28 15:45:53 -0400

    Read me

--- simple-ping-sweep.py
+++ simple-ping-sweep.py
@@ -8,6 +8,6 @@ for i in range(201,255):
 	try: 
 
 		sh.ping(address, "-c 1", _out="/dev/null")
-		print "ping tp", address, "OK"
+		print "ping to", address, "OK"
 	except sh.ErrorReturnCode_1:
 		print "no response from", address
